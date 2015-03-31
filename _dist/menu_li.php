538e43ef7e38de95b82a37243744a084e4e33711
Joel <dayjoaspen+github@gmail.com>
2015-03-31 19:36:03 +0100

    add default content, and rename some files

---
+++ _dist/menu_li.php
@@ -0,0 +1,30 @@
+<ul>
+	<li>
+		<a href='<?=ROOT;?>/films'>films</a>
+		<ul>
+			<li>
+				<a href='<?=ROOT;?>/films/films'>films</a>
+				<ul>
+					<li><a href='<?=ROOT;?>/films/films/godfather'>godfather</a></li>
+					<li><a href='<?=ROOT;?>/films/films/star-wars'>star wars</a></li>
+				</ul>
+			</li>
+			<li>
+				<a href='<?=ROOT;?>/films/music'>music</a>
+				<ul>
+					<li><a href='<?=ROOT;?>/films/music/ben-howard'>ben howard</a></li>
+					<li><a href='<?=ROOT;?>/films/music/coldplay-paradise'>coldplay paradise</a></li>
+					<li><a href='<?=ROOT;?>/films/music/metronomy'>metronomy</a></li>
+					<li><a href='<?=ROOT;?>/films/music/keaton-hensen'>keaton hensen</a></li>
+					<li><a href='<?=ROOT;?>/films/music/delilah'>delilah</a></li>
+					<li><a href='<?=ROOT;?>/films/music/emilie-sande'>emilie sande</a></li>
+					<li><a href='<?=ROOT;?>/films/music/coldcut'>coldcut</a></li>
+					<li><a href='<?=ROOT;?>/films/music/sonny-j'>sonny j</a></li>
+					<li><a href='<?=ROOT;?>/films/music/gorrillaz-feat-de-la-soul'>gorrillaz feat de la soul</a></li>
+				</ul>
+			</li>
+			<li><a href='<?=ROOT;?>/films/commercial'>commercial</a></li>
+			<li><a href='<?=ROOT;?>/films/contact'>contact</a></li>
+		</ul>
+	</li>
+</ul>
